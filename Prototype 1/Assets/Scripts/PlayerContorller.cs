0e3f86350a73a5a3391bcbcf9175439cd0bb955f
Tyler <foxkeh34@gmail.com>
2024-03-02 21:32:05 -0700

    Pedal to the Medal steps complete

--- Prototype 1/Assets/Scripts/PlayerContorller.cs
+++ Prototype 1/Assets/Scripts/PlayerContorller.cs
@@ -14,5 +14,6 @@ public class PlayerContorller : MonoBehaviour
     void Update()
     {
         // We'll move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
     }
 }
