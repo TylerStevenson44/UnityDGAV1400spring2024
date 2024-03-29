09163b8c037ae99291cf4064cbaaa83e5d7b665a
Tyler <foxkeh34@gmail.com>
2024-03-28 18:08:26 -0600

    Dont just stand there steps completed
    collision with fence still doesnt work when on the ground for too long.

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround;
@@ -13,6 +14,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
@@ -20,11 +22,12 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // use spacebar for jump and checks to see if the player has jumped
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) 
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver) 
         {
             //apply a force in the y axis immediately when space is pressed
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
     //onCollisionEnter method sets is on ground back to true when the player collider touches the ground again preventing double jumps
@@ -38,8 +41,10 @@ public class PlayerController : MonoBehaviour
         // we can use this same collision detection method to detect if we got a game over as well as preventing the double jump
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
-            gameOver = true;
             Debug.Log("Game Over");
+            gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
