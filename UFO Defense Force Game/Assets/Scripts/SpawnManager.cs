3f3e091a2ccf4a829dfb22b623626060d2a28605
Tyler <foxkeh34@gmail.com>
2024-04-07 20:23:57 -0600

    comments added

--- UFO Defense Force Game/Assets/Scripts/SpawnManager.cs
+++ UFO Defense Force Game/Assets/Scripts/SpawnManager.cs
@@ -15,6 +15,7 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //spawn the pickup repeatedly when our game starts.
         InvokeRepeating("SpawnPickup", startDelay, spawnInterval);
     }
 
